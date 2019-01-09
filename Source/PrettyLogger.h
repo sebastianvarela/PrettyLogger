91e3f134c2e5cbc74554a9589f896ebbbbd1ba0d
Sebastián Varela <sebastian.varela@s3ba.net>
2019-01-09 21:14:45 +0100

    Fix build for all targets

--- Source/PrettyLogger.h
+++ Source/PrettyLogger.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 S3BA. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for PrettyLogger.
 FOUNDATION_EXPORT double PrettyLoggerVersionNumber;

6018a2bb312839022a711d1fb66e90fdff9d6ba2
Sebastián Varela <sebastian.varela@s3ba.net>
2022-09-21 17:09:39 +0200

    Conform Equatable

--- Source/PrettyLoggerOutput.swift
+++ Source/PrettyLoggerOutput.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct PrettyLoggerOutput {
+public struct PrettyLoggerOutput: Equatable {
     public let level: PrettyLoggerLevel
     public let message: String
     public let file: String
